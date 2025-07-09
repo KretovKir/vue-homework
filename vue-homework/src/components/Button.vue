583746898260fbf64b4a1c9628222f8020cb9089
Kirill Kretov <KretovKir526@mail.ru>
2025-07-09 20:59:10 +0300

    fixed styles

--- vue-homework/src/components/Button.vue
+++ vue-homework/src/components/Button.vue
@@ -13,14 +13,14 @@
     height: 68px;
     padding: 10px 16px;
     border-radius: 100px;
-    font-family: 'Roboto';
+    font-family: var(--font);
     font-weight: 400;
     font-size: 24px;
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #008BFE;
-    color: #fff;
+    background-color: var(--color-btn-bg);
+    color: var(--color-primary);
     border: none;
   }
 </style>
