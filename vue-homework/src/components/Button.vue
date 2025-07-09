757efa97555668fa27e6513ae7d7e905559e6282
Kirill Kretov <KretovKir526@mail.ru>
2025-07-09 20:48:38 +0300

    2 homework complete

---
+++ vue-homework/src/components/Button.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+  .btn{
+    width: 335px;
+    height: 68px;
+    padding: 10px 16px;
+    border-radius: 100px;
+    font-family: 'Roboto';
+    font-weight: 400;
+    font-size: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #008BFE;
+    color: #fff;
+    border: none;
+  }
+</style>
